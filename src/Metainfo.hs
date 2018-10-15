4d8848161555f2e140da879eec0b90be7af47837
Ante <nikuda@gmail.com>
2018-10-15 22:28:23 +1100

    FileName and FileInfo fromBEncode instances

--- src/Metainfo.hs
+++ src/Metainfo.hs
@@ -1,19 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Metainfo where
 
-import Data.ByteString
-import Network.URI
+import Prelude hiding (length)
+import Data.ByteString (ByteString)
+import Data.BEncode
+import Network.URI (URI)
+import GHC.Generics
 
 newtype Announce = Announce URI
   deriving (Eq, Show)
 
 newtype FileName = FileName ByteString
-  deriving (Eq, Show)
+  deriving (Generic, Eq, Read, Show)
 
 newtype DirectoryName = DirectoryName ByteString
-  deriving (Eq, Show)
+  deriving (Eq, Read, Show)
 
 newtype SHA1 = SHA1 ByteString
-  deriving (Eq, Show)
+  deriving (Eq, Read, Show)
 
 data MetaInfo = MetaInfo
   { announce :: Announce
@@ -25,19 +31,35 @@ data Info = Info BaseInfo (Either FileInfo FilesInfo)
 data BaseInfo = BaseInfo
   { pieces :: [SHA1]
   , pieceLength :: Int
-  } deriving (Eq, Show)
+  } deriving (Eq, Read, Show)
 
 data FileInfo = FileInfo
   { name :: FileName
   , length :: Int
-  } deriving (Eq, Show)
+  } deriving (Generic, Eq, Read, Show)
 
 data FilesInfo = FilesInfo
   { directory :: DirectoryName
   , files :: [File]
-  } deriving (Eq, Show)
+  } deriving (Eq, Read, Show)
 
 data File = File
   { fileLength :: Int
   , filePath :: [DirectoryName] -- Last item is name of a file
-  } deriving (Eq, Show)
+  } deriving (Eq, Read, Show)
+
+
+--
+
+instance BEncode FileName where
+  fromBEncode = fromDict $ FileName <$>! "name"
+
+instance BEncode FileInfo where
+  toBEncode FileInfo { name = fiName, length = fiLength } = toDict $
+        "name"    .=! fiName
+    .: "length"   .=! fiLength
+    .: endDict
+
+  fromBEncode = fromDict $
+    FileInfo <$>! "name"
+             <*>! "length"
